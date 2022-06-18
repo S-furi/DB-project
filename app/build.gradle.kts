1abb192c0a844dd04b9704eeeb19434f7abddc9d
Stefano Furi <stefano.furi@studio.unibo.it>
2022-06-18 12:02:39 +0200

    added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    id("org.openjfx.javafxplugin") version "0.0.13"
 }
 
 repositories {
@@ -22,11 +23,23 @@ dependencies {
 
     // This dependency is used by the application.
     implementation("com.google.guava:guava:30.1.1-jre")
+    
+    // Used for connecting to mysql DB
+    implementation("mysql:mysql-connector-java:8.0.29")
+
+    // https://mvnrepository.com/artifact/org.json/json
+    implementation("org.json:json:20090211")
+    implementation("com.googlecode.json-simple:json-simple:1.1.1")
 }
 
 application {
     // Define the main class for the application.
-    mainClass.set("db.project.App")
+    mainClass.set("db_project.View.App")
+}
+
+javafx {
+    version = "17.0.2"
+    modules("javafx.controls", "javafx.fxml")
 }
 
 tasks.named<Test>("test") {
