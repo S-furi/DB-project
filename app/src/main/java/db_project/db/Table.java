4509341a29e62831218ae1b4b56e207f3557f255
Stefano Furi <stefano.furi@studio.unibo.it>
2022-06-18 12:30:36 +0200

    defines simple operations on a generic table

---
+++ app/src/main/java/db_project/db/Table.java
@@ -0,0 +1,65 @@
+package db_project.db;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Represents a DB table, with rows represented by objects of type V
+ * and primary key of type K.
+ * @param V the type of the objects saved in the table
+ * @param K the type of the primary key of the table
+ */
+public interface Table<V, K> {
+    /**
+     * 
+     * @return the name of the table
+     */
+    String getTableName();
+
+    /**
+     * Creates the DB table
+     * @return false if the table could not be created
+     */
+    boolean createTable();
+
+    /**
+     * Drops the DB table
+     * @return false if the table could not be dropped
+     */
+    boolean dropTable();
+
+    /**
+     * Finds an objet in the table with the given primary key
+     * @param primaryKey
+     * @return an empty {@link java.util.Optional} if there is no object
+     * with the given ID in the DB.
+     */
+    Optional<V> findByPrimaryKey(final K primaryKey);
+
+    /**
+     * 
+     * @return a list with all the rows of the DB
+     */
+    List<V> findAll();
+
+    /**
+     * Saves an object in the DB
+     * @param value
+     * @return
+     */
+    boolean save(final V value);
+
+    /**
+     * Updates an object
+     * @param updatedValue
+     * @return false if the object could not be updated
+     */
+    boolean update(final V updatedValue);
+
+    /**
+     * Deletes from the DB the row with the given primary key
+     * @param primaryKey
+     * @return false if the row could not be deleted
+     */
+    boolean delete(final K primaryKey);
+}
