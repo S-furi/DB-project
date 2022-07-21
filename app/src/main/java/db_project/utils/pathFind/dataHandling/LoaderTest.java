96fdf0e07e13f78252febfa332f52468e73a0007
Stefano Furi <stefano.furi@studio.unibo.it>
2022-07-21 20:49:12 +0200

    Included test for cached route info

--- app/src/main/java/db_project/utils/pathFind/dataHandling/LoaderTest.java
+++ app/src/main/java/db_project/utils/pathFind/dataHandling/LoaderTest.java
@@ -1,14 +1,29 @@
 package db_project.utils.pathFind.dataHandling;
 
+import java.util.List;
+import java.util.Map;
+
+import javafx.util.Pair;
+
 public class LoaderTest {
     private static StationsLoader stLoad;
     public static void main(String[] args) {
         stLoad = new StationsLoader();
-        printStations();
-        //stLoad.getDistancesData();
+        stLoad.cacheDistancesData();
+        printRouteInfo(stLoad.getRouteInfo());
     }
 
+    @SuppressWarnings ("unused")
     private static void printStations() {
         stLoad.getStations().forEach(System.out::println);
+    }
+
+    //@SuppressWarnings ("unused")
+    private static void printRouteInfo(final Map<String, List<Pair<String, Double>>> routeInfo) {
+        routeInfo.forEach((k, v) -> {
+            v.forEach(t -> {
+                System.out.println(String.format("(%s,%s):%f", k, t.getKey(), t.getValue()));
+            });
+        });
     }
 }
