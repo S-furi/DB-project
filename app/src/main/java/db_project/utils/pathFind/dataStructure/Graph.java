e3597c03cba8f3b0aee78a6c1013441370da52d1
Stefano Furi <stefano.furi@studio.unibo.it>
2022-07-21 18:01:13 +0200

    Simple Graph implementation

---
+++ app/src/main/java/db_project/utils/pathFind/dataStructure/Graph.java
@@ -0,0 +1,36 @@
+package db_project.utils.pathFind.dataStructure;
+
+import java.util.LinkedList;
+
+public class Graph {
+    private final int numOfVertices;
+    private final LinkedList<Edge>[] adjList;    
+    
+    /**
+     * Creates an empty graph.
+     * 
+     * @param numOfVertices
+     */
+    @SuppressWarnings("unchecked") //suppressing array type safety
+    public Graph(final int numOfVertices) {
+        this.numOfVertices = numOfVertices;
+        this.adjList = new LinkedList[numOfVertices];
+        
+        //initialize adjacency list for all the vertices
+        for (var i = 0; i < this.numOfVertices; i++) {
+            this.adjList[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int source, int destination, int weight) {
+        this.adjList[source].addFirst(new Edge(source, destination, weight));
+    }
+
+    public LinkedList<Edge>[] getAdjList() {
+        return this.adjList;
+    }
+
+    public int getNumOfVertices() {
+        return this.numOfVertices;
+    }
+}
