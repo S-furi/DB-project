ff8207943e3ffce6e8f923b3728356d6ce87b16e
Stefano Furi <stefano.furi@studio.unibo.it>
2022-07-22 00:11:08 +0200

    first test for Testing Route Pathfinding

---
+++ app/src/main/java/db_project/utils/pathFind/TestRoutePathFind.java
@@ -0,0 +1,8 @@
+package db_project.utils.pathFind;
+
+public class TestRoutePathFind {
+    public static void main(String[] args) {
+        RoutePathFinder pathFinder = new RoutePathFinder();
+        System.out.println(pathFinder.getTotalDistance("LAMEZIA", "PADOVA"));
+    }
+}
