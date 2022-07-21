a653166d1bf3d25f3f896fd0418a5006126bae58
Stefano Furi <stefano.furi@studio.unibo.it>
2022-07-21 18:02:58 +0200

    Simple test for PatFind

---
+++ app/src/main/java/db_project/utils/pathFind/TestPathFind.java
@@ -0,0 +1,30 @@
+package db_project.utils.pathFind;
+
+import db_project.utils.pathFind.dataStructure.Graph;
+
+public class TestPathFind {
+    public static void main(String[] args) {
+        final int numOfVertices = 6;
+        final int sourceVertex = 0;
+        final var g = new Graph(numOfVertices);
+
+        g.addEdge(0, 1, 4);
+		g.addEdge(0, 2, 3);
+		g.addEdge(1, 2, 1);
+		g.addEdge(1, 3, 2);
+		g.addEdge(2, 3, 4);
+		g.addEdge(3, 4, 2);
+		g.addEdge(4, 5, 6);
+        
+        PathFind pathFinder = new PathFind(g);
+        var distances = pathFinder.getMinDistance(sourceVertex);
+        printShortestPath(distances, numOfVertices, sourceVertex);
+    }
+
+    public static void printShortestPath(int[] distances, int numOfVertices, int sourceVertex) {
+        for(var i = 0; i < numOfVertices; i++) {
+            System.out.println("Source vertex: " + sourceVertex + "to vertex " 
+                        + i + ", distance: " + distances[i]);
+        }
+    }
+}
