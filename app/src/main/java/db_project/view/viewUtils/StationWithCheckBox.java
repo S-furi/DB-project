607c05c9e178881be611ee57acdf1513234e71c9
Stefano Furi <stefano.furi@studio.unibo.it>
2022-07-22 19:03:25 +0200

    Moved view utilities class inside this package

---
+++ app/src/main/java/db_project/view/viewUtils/StationWithCheckBox.java
@@ -0,0 +1,30 @@
+package db_project.view.viewUtils;
+
+import javafx.scene.control.CheckBox;
+
+public class StationWithCheckBox {
+    private String name;
+    private CheckBox select;
+    
+    public StationWithCheckBox(String name) {
+        this.name = name;
+        this.select = new CheckBox();
+        this.select.setSelected(true);
+    }
+
+    public CheckBox getSelect() {
+        return this.select;
+    }
+
+    public void setSelect(CheckBox select) {
+        this.select = select;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
