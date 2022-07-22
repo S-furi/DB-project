3b720f3d4899deeab3c7036986ef94ff309cb54b
Stefano Furi <stefano.furi@studio.unibo.it>
2022-07-22 09:53:22 +0200

    New lil test case

--- app/src/main/java/db_project/utils/pathFind/TestRoutePathFind.java
+++ app/src/main/java/db_project/utils/pathFind/TestRoutePathFind.java
@@ -3,6 +3,6 @@ package db_project.utils.pathFind;
 public class TestRoutePathFind {
     public static void main(String[] args) {
         RoutePathFinder pathFinder = new RoutePathFinder();
-        System.out.println(pathFinder.getTotalDistance("ROMA TERMINI", "PADOVA"));
+        System.out.println(pathFinder.getTotalDistance("BOLOGNA C.LE", "REGGIO EMILIA"));
     }
 }
