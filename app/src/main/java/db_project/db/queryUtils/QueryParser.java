942a970e83153fcb643c438023049a808792bfbf
Stefano Furi <stefano.furi@studio.unibo.it>
2022-07-26 14:20:10 +0200

    Modified interface in order to match changes in ArrayQueryParser

--- app/src/main/java/db_project/db/queryUtils/QueryParser.java
+++ app/src/main/java/db_project/db/queryUtils/QueryParser.java
@@ -1,26 +1,15 @@
 package db_project.db.queryUtils;
 
 import java.util.List;
+import java.util.Optional;
 
 import javafx.util.Pair;
 
 public interface QueryParser {
-    /**
-     * Execute the created query
-     * 
-     * @return a List of the results, mapped as a (key,value) pair
-     * where key is the name of the column, and value is the value of 
-     * the i-th row of the result.
-     */
-    public List<List<Pair<String, Object>>> getQueryResult();
 
-    /**
-     * 
-     * @param query
-     * @param params
-     * @return
-     */
-    public ArrayQueryParser insertQuery(final String query, final Object[] params);
+    public boolean computeSqlQuery(final String query, final Object[] params);
+
+    public Optional<List<List<Pair<String, Object>>>> getResult();
 
     public String getQuery();
 
