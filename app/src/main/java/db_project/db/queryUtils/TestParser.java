b8d34fd5739bf0b01ec0eae2233492b7ef2b1d6f
Stefano Furi <stefano.furi@studio.unibo.it>
2022-07-27 16:25:49 +0200

    Implemented simple tests callable from gradle test task

--- app/src/main/java/db_project/db/queryUtils/TestParser.java
+++ app/src/main/java/db_project/db/queryUtils/TestParser.java
@@ -5,16 +5,15 @@ import junit.framework.Assert;
 
 public class TestParser {
   private static final String TABLE_NAME = "users";
-
+  private static final QueryParser parser = new ArrayQueryParser(getConnection().getMySQLConnection());
   public static void main(String[] args) {
-    final var parser = new ArrayQueryParser(getConnection().getMySQLConnection());
     System.out.println("****(1)****");
     Assert.assertTrue("Simple Select Failed", testSelect(parser));
     System.out.println("****(2)****");
     Assert.assertTrue("InsertFailed", testInsert(parser));
   }
 
-  private static boolean testSelect(final QueryParser parser) {
+  public static boolean testSelect(final QueryParser parser) {
     parser.resetQuery();
     String query = String.format("SELECT * FROM %s WHERE id = ?", TABLE_NAME);
     Object[] params = {987426};
@@ -33,27 +32,27 @@ public class TestParser {
     return res;
   }
 
-  private static boolean testInsert(final QueryParser parser) {
+  public static boolean testInsert(final QueryParser parser) {
     parser.resetQuery();
     String query =
         String.format(
             "INSERT INTO %s (id, firstName, lastName, telephone, email) VALUES (?, ?, ?, ?, ?)",
             TABLE_NAME);
-    Object[] params = {1111, "Merda", "Dio", "1209849812", "merda@dio.paradise.org"};
+    Object[] params = {1111, "Luca", "Rapolla", "1209849812", "luca.rapolla@studio.unibo.it"};
 
     return parser.computeSqlQuery(query, params)
         == selectAll(parser)
         == deleteLastInsertedItem(parser, params[1]);
   }
 
-  private static boolean deleteLastInsertedItem(final QueryParser parser, Object object) {
+  public static boolean deleteLastInsertedItem(final QueryParser parser, Object object) {
     parser.resetQuery();
     String query = String.format("DELETE FROM %s WHERE id = ?", TABLE_NAME);
     Object[] params = {1111};
     return parser.computeSqlQuery(query, params);
   }
 
-  private static boolean selectAll(final QueryParser parser) {
+  public static boolean selectAll(final QueryParser parser) {
     parser.resetQuery();
     String query = String.format("SELECT * FROM %s", TABLE_NAME);
     boolean res = parser.computeSqlQuery(query, null);
@@ -63,10 +62,14 @@ public class TestParser {
     return res;
   }
 
-  private static ConnectionProvider getConnection() {
+  public static ConnectionProvider getConnection() {
     String username = "root";
     String password = "123Test123";
     String dbName = "users";
     return new ConnectionProvider(username, password, dbName);
+  }
+
+  public static QueryParser getParser() {
+    return parser;
   }
 }
