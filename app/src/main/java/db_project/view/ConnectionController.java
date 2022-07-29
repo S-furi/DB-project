dec3d2030ee0615715c642ee49614e8de614daf4
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-07-29 12:43:24 +0200

    Modify view

--- app/src/main/java/db_project/view/ConnectionController.java
+++ app/src/main/java/db_project/view/ConnectionController.java
@@ -4,7 +4,7 @@ import db_project.db.ConnectionProvider;
 
 public class ConnectionController {
     private static final String username = "root";
-    private static final String password = "123Test123";
+    private static final String password = "Spongebobluca01!";
     private static final String dbName = "users";
 
     public ConnectionController() {
