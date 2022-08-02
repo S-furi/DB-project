2c1855fda1def0b16b85ab2fce8799cb85a8d7c9
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-02 14:44:12 +0200

    Implemented simple PathInfo

--- app/src/main/java/db_project/model/PathInfo.java
+++ app/src/main/java/db_project/model/PathInfo.java
@@ -1,4 +1,42 @@
 package db_project.model;
 
 // Dettaglio Percorso
-public class PathInfo {}
+public class PathInfo {
+   private final String sectionId;
+   private final int orderNumber;
+   private final String pathId;
+
+   public PathInfo(
+           final String sectionId,
+           final int orderNumber,
+           final String pathId
+           ) {
+       this.sectionId = sectionId;
+       this.orderNumber = orderNumber;
+       this.pathId = pathId;
+
+   }
+
+   public String getSectionId() {
+       return this.sectionId;
+   }
+
+   public int getOrderNumber() {
+        return this.orderNumber;
+   }
+
+   public String getPathId() {
+       return this.pathId;
+   }
+
+   @Override
+   public String toString() {
+       return String.format(
+               "Section %s number %d of Path %s",
+                this.sectionId,
+                this.orderNumber,
+                this.pathId
+        );
+   }
+
+}
