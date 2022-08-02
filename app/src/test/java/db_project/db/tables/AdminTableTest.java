59fa44efa390e3d80f6660a2817131486a532c8e
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-02 17:30:55 +0200

    Changed Junit version in order to be called from `gradle test` task

--- app/src/test/java/db_project/db/tables/AdminTableTest.java
+++ app/src/test/java/db_project/db/tables/AdminTableTest.java
@@ -2,7 +2,7 @@ package db_project.db.tables;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import db_project.db.ConnectionProvider;
 
