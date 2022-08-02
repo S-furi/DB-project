928ef151d9226e415814f3245f4bca7a4c26117b
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-02 16:00:03 +0200

    Implemented FindByPrimaryKey

--- app/src/main/java/db_project/db/tables/AdminTable.java
+++ app/src/main/java/db_project/db/tables/AdminTable.java
@@ -1,18 +1,27 @@
 package db_project.db.tables;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 import db_project.db.Table;
+import db_project.db.queryUtils.ArrayQueryParser;
+import db_project.db.queryUtils.QueryParser;
+import db_project.db.queryUtils.QueryResult;
 import db_project.model.Admin;
+import javafx.util.Pair;
 
 public class AdminTable implements Table<Admin, Integer> {
     public static final String TABLE_NAME = "AMMINISTRATORE";
     private final Connection connection;
+    private final QueryParser queryParser;
 
     public AdminTable(final Connection connection) {
         this.connection = connection;
+        this.queryParser = new ArrayQueryParser(this.connection);
     }
 
     @Override
@@ -21,21 +30,16 @@ public class AdminTable implements Table<Admin, Integer> {
     }
 
     @Override
-    public boolean createTable() {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean dropTable() {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
     public Optional<Admin> findByPrimaryKey(Integer primaryKey) {
-        // TODO Auto-generated method stub
-        return Optional.empty();
+        String query = "SELECT * FROM " 
+                + TABLE_NAME 
+                + " WHERE adminID = ?";
+        Integer[] params = {primaryKey};
+        this.queryParser.computeSqlQuery(query, params);
+        
+        return this.readAdminsWithQueryResult(this.queryParser.getQueryResult())
+                    .stream()
+                    .findFirst();
     }
 
     @Override
@@ -60,6 +64,29 @@ public class AdminTable implements Table<Admin, Integer> {
     public boolean delete(Integer primaryKey) {
         // TODO Auto-generated method stub
         return false;
+    }
+
+    private List<Admin> readAdminsWithQueryResult(final QueryResult result) {
+        final List<Admin> admList = new ArrayList<>();
+        if (result.getResult().isEmpty()) {
+            throw new IllegalAccessError("Query Result is Empty");
+        }
+        result.getResult().get().forEach(res -> {
+            res.forEach(System.out::println);
+            final String id = (String)this.getResultColumnValue(res, t -> t.getKey().equals("adminID"));
+            final Date contractYear = (Date)this.getResultColumnValue(res, t -> t.getKey().equals("annoContratto"));
+            final String firstName = (String)this.getResultColumnValue(res, t -> t.getKey().equals("nome"));
+            final String lastName = (String)this.getResultColumnValue(res, t -> t.getKey().equals("cognome"));
+            final int telephone = (int)this.getResultColumnValue(res, t -> t.getKey().equals("telefono"));
+            final String email = (String)this.getResultColumnValue(res, t -> t.getKey().equals("email"));
+            final String residence = (String)this.getResultColumnValue(res, t -> t.getKey().equals("residenza"));
+            admList.add(new Admin(id, contractYear, firstName, lastName, telephone, email, residence));
+        });
+        return admList;
+    }
+
+    private Object getResultColumnValue(final List<Pair<String, Object>> res, Predicate<Pair<String, Object>> predicate) {
+        return res.stream().filter(t -> predicate.test(t)).findAny().get().getValue();
     }
     
 }
