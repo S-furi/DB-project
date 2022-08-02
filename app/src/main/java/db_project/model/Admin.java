34eb75c207ab06d55cc1fb84259fe2ac95509f7f
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-02 18:17:51 +0200

    Fixed String.format identifier

--- app/src/main/java/db_project/model/Admin.java
+++ app/src/main/java/db_project/model/Admin.java
@@ -89,7 +89,7 @@ public class Admin {
   @Override
   public String toString() {
     return String.format(
-        "(%d, %s, %s, %s, %d, %s, %s)",
+        "(%s, %s, %s, %s, %d, %s, %s)",
         this.id,
         this.firstName,
         this.lastName,
