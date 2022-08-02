82d6b3e1304040dec86d716f58c00b3e353c45ed
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-02 18:18:25 +0200

    Implemented simple test for `save()` method

--- app/src/test/java/db_project/db/tables/AdminTableTest.java
+++ app/src/test/java/db_project/db/tables/AdminTableTest.java
@@ -2,9 +2,13 @@ package db_project.db.tables;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
 
 import db_project.db.ConnectionProvider;
+import db_project.model.Admin;
+import db_project.utils.Utils;
 
 public class AdminTableTest {
   private static final String username = "root";
@@ -25,5 +29,20 @@ public class AdminTableTest {
   @Test
   public void testFindAll() {
     assertFalse(this.adminTable.findAll().isEmpty());
+  }
+
+  @Test
+  public void testSave() {
+    Date date = Utils.buildDate(12, 12, 2012).get();
+    Admin adm = new Admin(
+      "3",
+      Utils.dateToSqlDate(date), 
+      "stefano",
+      "furi",
+      35,
+      "stefano.furi7@gmail.com",
+      "C");
+    System.out.println(adm);
+    assertTrue(this.adminTable.save(adm));
   }
 }
