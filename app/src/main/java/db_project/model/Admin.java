fb90f403a4795b224ab59ef60d174bbd431e19eb
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-02 12:14:47 +0200

    Implemented simple Admin

--- app/src/main/java/db_project/model/Admin.java
+++ app/src/main/java/db_project/model/Admin.java
@@ -1,4 +1,73 @@
 package db_project.model;
 
+import java.util.Date;
 // Amministratore
-public class Admin {}
+public class Admin {
+    private final int id;
+    private final Date contractYear;
+    private final String firstName;
+    private final String lastName;
+    private final int telephone;
+    private final String email;
+    private final City residence;
+
+    public Admin(
+        final int id,
+        final Date contractYear,
+        final String firstName,
+        final String lastName,
+        final int telephone,
+        final String email,
+        final City residence
+    ) {
+        this.id = id;
+        this.contractYear = contractYear;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.telephone = telephone;
+        this.email = email;
+        this.residence = residence;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getContractYear() {
+        return contractYear;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public City getResidence() {
+        return residence;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "(%d, %s, %s, %s, %d, %s, %s)",
+                    this.id,
+                    this.firstName,
+                    this.lastName,
+                    this.contractYear.toString(),
+                    this.telephone,
+                    this.email,
+                    this.residence.toString());
+    }    
+
+}
