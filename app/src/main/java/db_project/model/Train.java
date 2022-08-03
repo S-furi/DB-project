d55f36a04cc050b68e1dd611aa99da91ac5dc6c9
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-08-03 19:07:51 +0200

    Implemented Train

--- app/src/main/java/db_project/model/Train.java
+++ app/src/main/java/db_project/model/Train.java
@@ -1,4 +1,44 @@
 package db_project.model;
 
 // Treno
-public class Train {}
+public class Train {
+
+    private final String trainCode;
+    private final String licenseNumber;
+    private final int capacity;
+    private final boolean isRegionaleVeloce;
+
+    public Train(String trainCode, String licenseNumber, int capacity, 
+            boolean isRegionaleVeloce) {
+        this.trainCode = trainCode;
+        this.licenseNumber = licenseNumber;
+        this.capacity = capacity;
+        this.isRegionaleVeloce = isRegionaleVeloce;
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public boolean isRegionaleVeloce() {
+        return isRegionaleVeloce;
+    }
+
+    @Override
+    public String toString() {
+        return "Train [capacity=" + capacity + ", isRegionaleVeloce=" 
+                + isRegionaleVeloce + ", licenseNumber="
+                + licenseNumber + ", trainCode=" + trainCode + "]";
+    }
+
+    
+    
+}
