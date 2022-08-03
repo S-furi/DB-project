9d6402552e37bb0f921579da7cdcb33cb8fe7317
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-08-03 19:03:34 +0200

    Implemented StationManager

--- app/src/main/java/db_project/model/StationManager.java
+++ app/src/main/java/db_project/model/StationManager.java
@@ -1,4 +1,66 @@
 package db_project.model;
 
+import java.sql.Date;
+
 // Amministratore Stazione
-public class StationManager {}
+public class StationManager {
+
+    private final String managerCode;
+    private final Date contractYear;
+    private final String firstName;
+    private final String lastName;
+    private final int phone;
+    private final String residence;
+
+    public StationManager(String managerCode, Date contractYear, 
+            String firstName, 
+            String lastName, 
+            int phone,
+            String residence) {
+        this.managerCode = managerCode;
+        this.contractYear = contractYear;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.residence = residence;
+    }
+
+    public String getManagerCode() {
+        return managerCode;
+    }
+
+    public Date getContractYear() {
+        return contractYear;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public String getResidence() {
+        return residence;
+    }
+
+    @Override
+    public String toString() {
+        return "StationManager [contractYear=" + contractYear + 
+                ", firstName=" + firstName + ", lastName=" + lastName
+                + ", managerCode=" + managerCode + ", phone=" + phone + 
+                ", residence=" + residence + "]";
+    }
+
+    
+
+    
+
+
+
+}
