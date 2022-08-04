3e19d18f099da4e97b582178cfa1ca59aceabe2e
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-08-04 12:13:09 +0200

    Implemented Section, might need a refactor

--- app/src/main/java/db_project/model/Section.java
+++ app/src/main/java/db_project/model/Section.java
@@ -1,4 +1,40 @@
 package db_project.model;
 
 // Tratta
-public class Section {}
+public class Section {
+
+    private final String startStation;
+    private final String endStation;
+    private final String sectionCode;
+    private final int distance;
+    private final String routeCode;
+    
+    public Section(String startStation, String endStation, String sectionCode, int distance, String routeCode) {
+        this.startStation = startStation;
+        this.endStation = endStation;
+        this.sectionCode = sectionCode;
+        this.distance = distance;
+        this.routeCode = routeCode;
+    }
+
+    public String getStartStation() {
+        return startStation;
+    }
+
+    public String getEndStation() {
+        return endStation;
+    }
+
+    public String getSectionCode() {
+        return sectionCode;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public String getRouteCode() {
+        return routeCode;
+    }
+
+}
