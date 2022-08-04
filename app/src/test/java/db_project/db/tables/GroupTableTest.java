1354f053c171a27405b1c4eaf82bb349824f90e4
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-04 17:44:07 +0200

    tests for `findAll()` and `findByPrimaryKey()`

--- app/src/test/java/db_project/db/tables/GroupTableTest.java
+++ app/src/test/java/db_project/db/tables/GroupTableTest.java
@@ -4,8 +4,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import db_project.db.ConnectionProvider;
+import db_project.model.Group;
+
 
 public class GroupTableTest {
+    private static final String username = "root";
+    private static final String password = "123Test123";
+    private static final String dbName = "Ferrovia";
+  
+    private final ConnectionProvider connectionProvider =
+        new ConnectionProvider(username, password, dbName);
+
+    private final GroupTable groupTable =
+        new GroupTable(this.connectionProvider.getMySQLConnection());
+
+    private final Group group = new Group("3", 13);
+    
     @Test
     public void testDelete() {
 
@@ -13,12 +28,14 @@ public class GroupTableTest {
 
     @Test
     public void testFindAll() {
-
+        assertFalse(this.groupTable.findAll().isEmpty());
+        assertTrue(this.groupTable.findAll().size() == 2);
     }
 
     @Test
     public void testFindByPrimaryKey() {
-
+        assertTrue(this.groupTable.findByPrimaryKey("1").isPresent());
+        assertFalse(this.groupTable.findByPrimaryKey("8").isPresent());
     }
 
     @Test
