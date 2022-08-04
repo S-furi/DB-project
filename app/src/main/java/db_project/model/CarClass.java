b2fbf971e3ff38e751e86a5fd510cf59b114cf2f
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-08-04 11:51:50 +0200

    Implemented CarClass

--- app/src/main/java/db_project/model/CarClass.java
+++ app/src/main/java/db_project/model/CarClass.java
@@ -1,4 +1,22 @@
 package db_project.model;
 
 // Classe
-public class CarClass {}
+public class CarClass {
+
+    private int classType;
+    private int availableSeats;
+
+    public CarClass(int classType, int availableSeats){
+        this.classType = classType;
+        this.availableSeats = availableSeats;
+    }
+
+    int getClassType(){
+        return this.classType;
+    }
+
+    int getAvailableSeats(){
+        return this.availableSeats;
+    }
+    
+}
