20dbe862575b49aabbd1bcb5da6bfdbba45a95d9
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-08-04 12:15:25 +0200

    Implemented Subscription

--- app/src/main/java/db_project/model/Subscription.java
+++ app/src/main/java/db_project/model/Subscription.java
@@ -1,4 +1,30 @@
 package db_project.model;
 
+import java.sql.Date;
+
 // Sottoscrizione
-public class Subscription {}
+public class Subscription {
+
+    private final String travelerCode;
+    private final String cardNumber;
+    private final Date subscriptionDate;
+    
+    public Subscription(String travelerCode, String cardNumber, Date subscriptionDate) {
+        this.travelerCode = travelerCode;
+        this.cardNumber = cardNumber;
+        this.subscriptionDate = subscriptionDate;
+    }
+
+    public String getTravelerCode() {
+        return travelerCode;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public Date getSubscriptionDate() {
+        return subscriptionDate;
+    }
+
+}
