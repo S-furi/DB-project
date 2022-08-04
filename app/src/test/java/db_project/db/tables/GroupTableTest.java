2aed3b4d9515cf4b9b628b491d2d5039dfc75aed
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-04 17:17:17 +0200

    Created `Group`s tests

---
+++ app/src/test/java/db_project/db/tables/GroupTableTest.java
@@ -0,0 +1,33 @@
+package db_project.db.tables;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+public class GroupTableTest {
+    @Test
+    public void testDelete() {
+
+    }
+
+    @Test
+    public void testFindAll() {
+
+    }
+
+    @Test
+    public void testFindByPrimaryKey() {
+
+    }
+
+    @Test
+    public void testSave() {
+
+    }
+
+    @Test
+    public void testUpdate() {
+
+    }
+}
