c0442958e68144dfb25803094de5d2d81a098191
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-04 18:18:07 +0200

    Refactored due to change of QueryResult data types

--- app/src/main/java/db_project/db/tables/AdminTable.java
+++ app/src/main/java/db_project/db/tables/AdminTable.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Predicate;
 
 import db_project.db.Table;
 import db_project.db.queryUtils.ArrayQueryParser;
@@ -14,7 +13,6 @@ import db_project.db.queryUtils.QueryParser;
 import db_project.db.queryUtils.QueryResult;
 import db_project.model.Admin;
 import db_project.utils.Utils;
-import javafx.util.Pair;
 
 public class AdminTable implements Table<Admin, String> {
   public static final String TABLE_NAME = "AMMINISTRATORE";
@@ -33,27 +31,28 @@ public class AdminTable implements Table<Admin, String> {
 
   @Override
   public Optional<Admin> findByPrimaryKey(final String primaryKey) {
-    String query = "SELECT * FROM " + TABLE_NAME + " WHERE adminID = ?";
-    String[] params = {primaryKey};
+    final String query = "SELECT * FROM " + TABLE_NAME + " WHERE adminID = ?";
+    final String[] params = {primaryKey};
     this.queryParser.computeSqlQuery(query, params);
     return this.readAdminsWithQueryResult(this.queryParser.getQueryResult()).stream().findFirst();
   }
 
   @Override
   public List<Admin> findAll() {
-    String query = "SELECT * FROM " + TABLE_NAME;
+    final String query = "SELECT * FROM " + TABLE_NAME;
     this.queryParser.computeSqlQuery(query, null);
     return this.readAdminsWithQueryResult(this.queryParser.getQueryResult());
   }
 
   @Override
   public boolean save(final Admin admin) {
-    String query =
+    final String query =
         "INSERT INTO "
             + TABLE_NAME
             + " (adminID, annoContratto, nome, cognome, telefono, email, residenza)"
             + "VALUES (?, ?, ?, ?, ?, ?, ?)";
-    Object[] params = {
+
+    final Object[] params = {
       admin.getId(),
       Utils.dateToSqlDate(admin.getContractYear()),
       admin.getFirstName(),
@@ -63,10 +62,7 @@ public class AdminTable implements Table<Admin, String> {
       admin.getResidence()
     };
 
-    if (this.queryParser.computeSqlQuery(query, params)) {
-      return true;
-    }
-    return false;
+    return this.queryParser.computeSqlQuery(query, params);
   }
 
   @Override
@@ -90,7 +86,7 @@ public class AdminTable implements Table<Admin, String> {
             + "residenza = ?"
             + " WHERE adminID = ?";
 
-    Object[] params = {
+    final Object[] params = {
       Utils.dateToSqlDate(admin.getContractYear()),
       admin.getFirstName(),
       admin.getLastName(),
@@ -112,30 +108,18 @@ public class AdminTable implements Table<Admin, String> {
         .getResult()
         .get()
         .forEach(
-            res -> {
-              res.forEach(System.out::println);
-              final String id =
-                  (String) this.getResultColumnValue(res, t -> t.getKey().equals("adminID"));
-              final Date contractYear =
-                  (Date) this.getResultColumnValue(res, t -> t.getKey().equals("annoContratto"));
-              final String firstName =
-                  (String) this.getResultColumnValue(res, t -> t.getKey().equals("nome"));
-              final String lastName =
-                  (String) this.getResultColumnValue(res, t -> t.getKey().equals("cognome"));
-              final int telephone =
-                  (int) this.getResultColumnValue(res, t -> t.getKey().equals("telefono"));
-              final String email =
-                  (String) this.getResultColumnValue(res, t -> t.getKey().equals("email"));
-              final String residence =
-                  (String) this.getResultColumnValue(res, t -> t.getKey().equals("residenza"));
+            row -> {
+              System.out.println(row.toString());
+              final String id = (String) row.get("adminID");
+              final Date contractYear = (Date) row.get("annoContratto");
+              final String firstName = (String) row.get("nome");
+              final String lastName = (String) row.get("cognome");
+              final int telephone = (int) row.get("telefono");
+              final String email = (String) row.get("email");
+              final String residence = (String) row.get("residenza");
               admList.add(
                   new Admin(id, contractYear, firstName, lastName, telephone, email, residence));
             });
     return admList;
-  }
-
-  private Object getResultColumnValue(
-      final List<Pair<String, Object>> res, Predicate<Pair<String, Object>> predicate) {
-    return res.stream().filter(t -> predicate.test(t)).findAny().get().getValue();
   }
 }
