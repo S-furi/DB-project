36f3919623268a8784144dca5508d2075572cece
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-08-04 12:05:05 +0200

    Implemented Path

--- app/src/main/java/db_project/model/Path.java
+++ app/src/main/java/db_project/model/Path.java
@@ -1,4 +1,34 @@
 package db_project.model;
 
 // Percorso
-public class Path {}
+public class Path {
+
+    private final String pathCode;
+    private final String totalTime;
+    private final int stops;
+    private final String adminID;
+
+    public Path(String pathCode, String totalTime, int stops, String adminID){
+        this.pathCode = pathCode;
+        this.totalTime = totalTime;
+        this.stops = stops;
+        this.adminID = adminID;
+    }
+
+    public String getPathCode() {
+        return pathCode;
+    }
+
+    public String getTotalTime() {
+        return totalTime;
+    }
+
+    public int getStops() {
+        return stops;
+    }
+
+    public String getAdminID() {
+        return adminID;
+    }
+
+}
