4716b1ffa6820f9a181b0b0226b5cafab80f327e
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-08-04 12:08:38 +0200

    Implemented Seat

--- app/src/main/java/db_project/model/Seat.java
+++ app/src/main/java/db_project/model/Seat.java
@@ -1,4 +1,34 @@
 package db_project.model;
 
 // Posto
-public class Seat {}
+public class Seat {
+
+    private final int classType;
+    private final String trainCode;
+    private final int carNumber;
+    private final int seatNumber;
+
+    public Seat(int classType, String trainCode, int carNumber, int seatNumber){
+        this.classType = classType;
+        this.trainCode = trainCode;
+        this.carNumber = carNumber;
+        this.seatNumber = seatNumber;
+    }
+
+    public int getClassType() {
+        return classType;
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public int getCarNumber() {
+        return carNumber;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+    
+}
