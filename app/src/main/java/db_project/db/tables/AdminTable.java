09db8d52ddcce31cb722f27bda3b3f94baf45ecb
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-06 12:54:18 +0200

    Fixed java.sql.Date parsing

--- app/src/main/java/db_project/db/tables/AdminTable.java
+++ app/src/main/java/db_project/db/tables/AdminTable.java
@@ -9,46 +9,51 @@ import java.util.List;
 import db_project.db.AbstractTable;
 import db_project.db.queryUtils.QueryResult;
 import db_project.model.Admin;
+import db_project.utils.Utils;
 
 public class AdminTable extends AbstractTable<Admin, String> {
-    public static final String TABLE_NAME = "AMMINISTRATORE";
-    public static final String PRIMARY_KEY = "adminID";
+  public static final String TABLE_NAME = "AMMINISTRATORE";
+  public static final String PRIMARY_KEY = "adminID";
 
-    public AdminTable(final Connection connection) {
-        super(TABLE_NAME, connection);
-        super.setPrimaryKey(PRIMARY_KEY);
-        super.setTableColumns(List.of("annoContratto", "nome", "cognome", "telefono", "email", "residenza"));
-    }
+  public AdminTable(final Connection connection) {
+    super(TABLE_NAME, connection);
+    super.setPrimaryKey(PRIMARY_KEY);
+    super.setTableColumns(List.of("annoContratto", "nome", "cognome", "telefono", "email", "residenza"));
+  }
 
-    @Override
-    protected Object[] getSaveQueryParameters(final Admin admin) {
-        return new Object[] { admin.getId(), admin.getContractYear(), admin.getFirstName(), admin.getLastName(),
-                admin.getTelephone(), admin.getEmail(), admin.getResidence() };
-    }
+  @Override
+  protected Object[] getSaveQueryParameters(final Admin admin) {
+    return new Object[] { admin.getId(), Utils.dateToSqlDate(admin.getContractYear()), admin.getFirstName(),
+        admin.getLastName(),
+        admin.getTelephone(), admin.getEmail(), admin.getResidence() };
+  }
 
-    @Override
-    protected Object[] getUpdateQueryParameters(final Admin admin) {
-        return new Object[] { admin.getContractYear(), admin.getFirstName(), admin.getLastName(), admin.getTelephone(),
-                admin.getEmail(), admin.getResidence(), admin.getId() };
-    }
+  @Override
+  protected Object[] getUpdateQueryParameters(final Admin admin) {
+    return new Object[] { Utils.dateToSqlDate(admin.getContractYear()), admin.getFirstName(), admin.getLastName(),
+        admin.getTelephone(),
+        admin.getEmail(), admin.getResidence(), admin.getId() };
+  }
 
-    @Override
-    protected List<Admin> getPrettyResultFromQueryResult(final QueryResult result) {
-        if (result.getResult().isEmpty()) {
-            return Collections.emptyList();
-        }
-        final List<Admin> admList = new ArrayList<>();
-        result.getResult().get().forEach(row -> {
-            System.out.println(row.toString());
-            final String id = (String) row.get("adminID");
-            final Date contractYear = (Date) row.get("annoContratto");
-            final String firstName = (String) row.get("nome");
-            final String lastName = (String) row.get("cognome");
-            final int telephone = (int) row.get("telefono");
-            final String email = (String) row.get("email");
-            final String residence = (String) row.get("residenza");
-            admList.add(new Admin(id, contractYear, firstName, lastName, telephone, email, residence));
-        });
-        return admList;
+  @Override
+  protected List<Admin> getPrettyResultFromQueryResult(final QueryResult result) {
+    if (result.getResult().isEmpty()) {
+      return Collections.emptyList();
     }
+    final List<Admin> admList = new ArrayList<>();
+
+    result.getResult().get().forEach(row -> {
+      System.out.println(row.toString());
+      final String id = (String) row.get("adminID");
+      final Date contractYear = (Date) row.get("annoContratto");
+      final String firstName = (String) row.get("nome");
+      final String lastName = (String) row.get("cognome");
+      final int telephone = (int) row.get("telefono");
+      final String email = (String) row.get("email");
+      final String residence = (String) row.get("residenza");
+      admList.add(new Admin(id, contractYear, firstName, lastName, telephone, email, residence));
+    });
+    
+    return admList;
+  }
 }
