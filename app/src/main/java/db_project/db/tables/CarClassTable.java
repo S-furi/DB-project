994211dd0d50b2850b17dda5612344a3e6608f19
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-08-08 15:42:46 +0200

    Implemented CarClassTable

---
+++ app/src/main/java/db_project/db/tables/CarClassTable.java
@@ -0,0 +1,55 @@
+package db_project.db.tables;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import db_project.db.AbstractTable;
+import db_project.db.queryUtils.QueryResult;
+import db_project.model.CarClass;
+
+public class CarClassTable extends AbstractTable<CarClass, String>{
+
+    public static String TABLE_NAME = "CLASSE";
+    public static String primary_key = "numClasse";
+
+    public CarClassTable(final Connection connection){
+        super(TABLE_NAME, connection);
+        super.setPrimaryKey(primary_key);
+        super.setTableColumns(List.of("postiDisponibili"));
+    }
+
+
+    @Override
+    protected Object[] getSaveQueryParameters(CarClass carClass) {
+        return new Object[]{
+            carClass.getClassType(), 
+            carClass.getAvailableSeats()
+        };
+    }
+
+    @Override
+    protected Object[] getUpdateQueryParameters(CarClass carclass) {
+        return new Object[]{
+            carclass.getClassType(),
+            carclass.getAvailableSeats()
+        };
+    }
+
+    @Override
+    protected List<CarClass> getPrettyResultFromQueryResult(QueryResult result) {
+        if(result.getResult().isEmpty()){
+            return Collections.emptyList();
+        }
+        List<CarClass> carClasses = new ArrayList<>();
+        result.getResult().get().forEach(row -> {
+            System.out.println(row.toString());
+            final int classNum = (int) row.get("numClasse");
+            final int availableSeats = (int) row.get("postiDisponibili");
+            carClasses.add(new CarClass(classNum, availableSeats));
+        });
+        return carClasses;
+    }
+    
+}
