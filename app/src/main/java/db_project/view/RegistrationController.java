0d8fbbce1f9ff03d05d63784b598479fb82b6960
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-08-08 17:30:16 +0200

    Handling the registration phase

--- app/src/main/java/db_project/view/RegistrationController.java
+++ app/src/main/java/db_project/view/RegistrationController.java
@@ -3,8 +3,13 @@ package db_project.view;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
+import db_project.db.ConnectionProvider;
+import db_project.db.tables.CityTable;
+import db_project.db.tables.PassengerTable;
+import db_project.model.Passenger;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -43,11 +48,21 @@ public class RegistrationController implements Initializable {
   private List<String> accounts;
   private List<String> data;
 
+  private static String username = "root";
+  private static String password = "123Test123";
+  private static String db_name = "Ferrovia";
+
+  private static final ConnectionProvider connectionProvider = new ConnectionProvider(username, password, db_name);
+  private static final CityTable cityTable = new CityTable(connectionProvider.getMySQLConnection());
+  private static final PassengerTable passengerTable = new PassengerTable(connectionProvider.getMySQLConnection());
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     this.populateAccountTypes();
     this.populateCountryData();
 
+
+    this.populateBoxes();
     this.accountTypes
         .getSelectionModel()
         .selectedIndexProperty()
@@ -91,14 +106,35 @@ public class RegistrationController implements Initializable {
     this.data.add(regReg.getValue());
     this.data.add(provReg.getValue());
     this.data.add(cittReg.getText());
+    
+    if(this.accountTypes.getValue() == "Utente"){
+      if(this.cartArrowReg.isSelected()){
+        this.data.add("true");
+      }else{
+        this.data.add("false");
+      }
+    }
+
+
 
     // System.out.println(nameReg.getText().isEmpty());
-    // System.out.println(this.data.toString());
     if (!this.validateData()) {
+      System.out.println("DATI NON CORRETTI");
       this.nameReg.clear();
       this.surnameReg.clear();
       this.phoneNumReg.clear();
       this.emailReg.clear();
+    }
+
+    this.executeData(data);
+  }
+
+  private void executeData(List<String> data){
+    var userType = data.get(0);
+    if(userType == "Utente"){
+      var newID = passengerTable.getHighestID() + 1;
+      var newUser = new Passenger(Integer.toString(newID), data.get(2), data.get(3), Integer.parseInt(data.get(4)), data.get(5), "Residenza", Optional.of("1"));
+      System.out.println(newUser.toString());
     }
   }
 
@@ -106,4 +142,14 @@ public class RegistrationController implements Initializable {
     boolean allNotEmpty = this.data.stream().allMatch(x -> x.isEmpty() == false);
     return allNotEmpty;
   }
+
+  private void populateBoxes(){
+    cityTable.findAll().forEach(t -> {
+      this.regReg.getItems().add(t.getRegion());
+      this.provReg.getItems().add(t.getProvince());
+    });
+    System.out.println("ChoiceBox setup went fine");
+  }
+
+
 }
