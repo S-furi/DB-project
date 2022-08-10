3d54beae805d485af2c636a49b296ad42ce3239f
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-10 12:02:31 +0200

    Implement Train DB Table

---
+++ app/src/main/java/db_project/db/tables/TrainTable.java
@@ -0,0 +1,56 @@
+package db_project.db.tables;
+
+import java.sql.Connection;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+import db_project.db.AbstractTable;
+import db_project.db.queryUtils.QueryResult;
+import db_project.model.Train;
+
+public class TrainTable extends AbstractTable<Train, String> {
+    public static final String TABLE_NAME = "TRENO";
+    public static final String PRIMARY_KEY = "codTreno";
+
+    public TrainTable(final Connection connection) {
+        super(TABLE_NAME, connection);
+        super.setPrimaryKey(PRIMARY_KEY);
+        super.setTableColumns(List.of("codMacchinista", "capienza", "regionaleVeloce"));
+
+    }
+
+    @Override
+    protected Object[] getSaveQueryParameters(final Train train) {
+        return new Object[] { train.getTrainCode(), train.getLicenseNumber(), train.getCapacity(),
+                train.isRegionaleVeloce() ? "1" : "0" };
+    }
+
+    @Override
+    protected Object[] getUpdateQueryParameters(final Train train) {
+        return new Object[] {
+            train.getLicenseNumber(),
+            train.getCapacity(),
+            train.isRegionaleVeloce() ? "1" : "0",
+            train.getTrainCode()
+        };
+    }
+
+    @Override
+    protected List<Train> getPrettyResultFromQueryResult(final QueryResult result) {
+        if (result.getResult().isEmpty()) {
+            return Collections.emptyList();
+        }
+        List<Train> trains = new ArrayList<>();
+        result.getResult().get().forEach(row -> {
+            System.out.println(row.toString());
+            final String trainCode = (String) row.get("codTreno");
+            final String licenseNumber = (String) row.get("codMacchinista");
+            final int capacity = (int) row.get("capienza");
+            final boolean isRegionaleVeloce = ((String) row.get("regionaleVeloce")).equals("0") ? false : true;
+            trains.add(new Train(trainCode, licenseNumber, capacity, isRegionaleVeloce));
+        });
+        return trains;
+    }
+
+}
