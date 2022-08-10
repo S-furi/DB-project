d00035ebcab0c48ae37f175cb52edd17c30ac893
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-08-10 20:19:25 +0200

    Implemented Station Tests

--- app/src/test/java/db_project/db/tables/StationTableTest.java
+++ app/src/test/java/db_project/db/tables/StationTableTest.java
@@ -1,7 +1,9 @@
 package db_project.db.tables;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterAll;
@@ -21,11 +23,14 @@ public class StationTableTest {
   private static final StationTable stationTable =
       new StationTable(connectionProvider.getMySQLConnection());
 
+  private final Station station3 = new Station("3", "D", 2, "2");
+  
+
   @BeforeAll
   static void setUp() {
     StationManagerTableTest.setUp();
-    final Station station1 = new Station("1", "C", 6, "121");
-    final Station station2 = new Station("2", "C", 2, "212");
+    final Station station1 = new Station("1", "C", 6, "1");
+    final Station station2 = new Station("2", "C", 2, "2");
 
     assertTrue(stationTable.save(station1));
     assertTrue(stationTable.save(station2));
@@ -41,5 +46,31 @@ public class StationTableTest {
   public void testFindByPrimaryKey() {
     assertTrue(stationTable.findByPrimaryKey("1").isPresent());
     assertFalse(stationTable.findByPrimaryKey("3").isPresent());
+  }
+
+  @Test
+  public void testFindAll(){
+    assertFalse(stationTable.findAll().isEmpty());
+    assertTrue(stationTable.findAll().size() == 2);
+  }
+
+  @Test
+  public void testSaveAndDelete(){
+    assertTrue(stationTable.save(this.station3));
+    assertThrows(IllegalStateException.class, 
+      () -> {
+        stationTable.save(this.station3);
+      });
+    assertTrue(stationTable.delete(this.station3.getStationCode()));
+    assertFalse(stationTable.delete(this.station3.getStationCode()));
+  }
+
+  @Test
+  public void testUpdate(){
+    final var currStation = stationTable.findByPrimaryKey("1");
+    if(currStation.isEmpty()){
+      fail("Select Failed");
+    }
+    assertTrue(stationTable.update(new Station("1", this.station3.getStationName(), this.station3.getRails(), this.station3.getManagerCode())));
   }
 }
