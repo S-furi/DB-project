ec22268788a1c3cd9c08ab1bf51d374d415668f8
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-12 14:37:48 +0200

    Added tests for Admin, Driver and Managers.

--- app/src/test/java/db_project/db/dataLoading/DataLoadingTest.java
+++ app/src/test/java/db_project/db/dataLoading/DataLoadingTest.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 import db_project.db.dbGenerator.DBGenerator;
+import db_project.db.tables.AdminTable;
 import db_project.db.tables.CityTable;
+import db_project.db.tables.DriverTable;
+import db_project.db.tables.StationManagerTable;
 
 import org.junit.jupiter.api.BeforeAll;
 
@@ -18,6 +21,13 @@ public class DataLoadingTest {
   public static void setUp() {
     dbGenerator.createDB();
     dbGenerator.createTables();
+    createCitiesDependency();
+  }
+  
+  private static void createCitiesDependency() {
+    final CityTable cityTable = (CityTable) dbGenerator.getTableByClass(CityTable.class);
+    final var cities = cityTable.readFromFile();
+    cities.forEach(t -> cityTable.save(t));
   }
 
   @AfterAll
@@ -26,14 +36,33 @@ public class DataLoadingTest {
   }
 
   @Test
-  public void testCityReadAndInsertion() {
-    final var optCityTable =
-        dbGenerator.getTables().stream()
-            .filter(t -> t.getClass().equals(CityTable.class))
-            .findAny();
-    assertTrue(optCityTable.isPresent());
-    final CityTable cityTable = (CityTable) optCityTable.get();
-    final var cities = cityTable.readFromFile();
-    cities.forEach(t -> assertTrue(cityTable.save(t)));
+  public void testCityReadAndStored() {
+    final CityTable cityTable = (CityTable) dbGenerator.getTableByClass(CityTable.class);
+    assertTrue(cityTable.findAll().size() == cityTable.readFromFile().size());
+  }
+
+
+  @Test
+  public void testAdminReadAndInsertion() {
+    final AdminTable adminTable = (AdminTable) dbGenerator.getTableByClass(AdminTable.class);
+    final var admins = adminTable.readFromFile();
+    admins.forEach(t -> assertTrue(adminTable.save(t)));
+    assertTrue(adminTable.findAll().size() == admins.size());
+  }
+
+  @Test
+  public void testDriverReadAndInsertion() {
+    final DriverTable driverTable = (DriverTable) dbGenerator.getTableByClass(DriverTable.class);
+    final var drivers = driverTable.readFromFile();
+    drivers.forEach(t -> assertTrue(driverTable.save(t)));
+    assertTrue(driverTable.findAll().size() == drivers.size());
+  }
+
+  @Test
+  public void testStationManagersReadAndInsertion() {
+    final StationManagerTable stationManagerTable = (StationManagerTable) dbGenerator.getTableByClass(StationManagerTable.class);
+    final var managers = stationManagerTable.readFromFile();
+    managers.forEach(t -> assertTrue(stationManagerTable.save(t)));
+    assertTrue(stationManagerTable.findAll().size() == managers.size());
   }
 }
