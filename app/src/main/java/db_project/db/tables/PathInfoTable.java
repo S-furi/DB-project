b0b9fbdd2c7b5271d9534f3490d830e16aa8d398
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-13 12:09:31 +0200

    Now PathInfo has a composite key
    (now it's extending `AbstractCompositeKeyTable`)

--- app/src/main/java/db_project/db/tables/PathInfoTable.java
+++ app/src/main/java/db_project/db/tables/PathInfoTable.java
@@ -7,44 +7,51 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import db_project.db.AbstractTable;
+import db_project.db.AbstractCompositeKeyTable;
 import db_project.db.JsonReadeable;
 import db_project.db.queryUtils.QueryResult;
 import db_project.model.PathInfo;
 import db_project.utils.AbstractJsonReader;
 
-public class PathInfoTable extends AbstractTable<PathInfo, String>
+public class PathInfoTable extends AbstractCompositeKeyTable<PathInfo, Object>
     implements JsonReadeable<PathInfo> {
   public static final String TABLE_NAME = "DETTAGLIO_PERCORSO";
-  public static final String PRIMARY_KEY = "codTratta";
+  public static final List<String> PRIMARY_KEY = List.of("codPercorso", "ordine");
   private final Logger logger;
 
   public PathInfoTable(final Connection connection) {
     super(TABLE_NAME, connection);
     super.setPrimaryKey(PRIMARY_KEY);
-    super.setTableColumns(List.of("ordine", "codPercorso"));
+    super.setTableColumns(List.of("codTratta"));
     this.logger = Logger.getLogger("CityTable");
     this.logger.setLevel(Level.WARNING);
   }
 
   @Override
   protected Object[] getSaveQueryParameters(final PathInfo pathInfo) {
-    return new Object[] {pathInfo.getSectionId(), pathInfo.getOrderNumber(), pathInfo.getPathId()};
+    return new Object[] {pathInfo.getPathId(), pathInfo.getOrderNumber(), pathInfo.getSectionId()};
   }
 
   @Override
   protected Object[] getUpdateQueryParameters(final PathInfo pathInfo) {
-    return new Object[] {pathInfo.getOrderNumber(), pathInfo.getPathId(), pathInfo.getSectionId()};
+    return new Object[] {
+      pathInfo.getPathId(),
+      pathInfo.getOrderNumber(),
+      pathInfo.getPathId(),
+      pathInfo.getOrderNumber(),
+      pathInfo.getSectionId()};
   }
 
   @Override
   public boolean createTable() {
     final String query =
-        "create table DETTAGLIO_PERCORSO ( "
-            + "codTratta varchar(5) not null, "
-            + "ordine int not null, "
-            + "codPercorso varchar(5) not null, "
-            + "constraint FKStr_TRA_ID primary key (codTratta));";
+      "create table DETTAGLIO_PERCORSO ("
+        + "codPercorso varchar(5) not null,"
+        + "ordine char(10) not null,"
+        + "codTratta varchar(5) not null,"
+        + "constraint ID_Strutturazione_ID primary key (codPercorso, ordine),"
+        + "constraint FKStr_TRA_ID unique (codTratta));";
+            
     super.created = super.parser.computeSqlQuery(query, null);
     return super.isCreated();
   }
@@ -62,7 +69,7 @@ public class PathInfoTable extends AbstractTable<PathInfo, String>
             row -> {
               logger.info(row.toString());
               final String sectionId = (String) row.get("codTratta");
-              final int orderNumber = (int) row.get("ordine");
+              final String orderNumber = (String) row.get("ordine");
               final String pathId = (String) row.get("codPercorso");
               pathInfos.add(new PathInfo(sectionId, orderNumber, pathId));
             });
