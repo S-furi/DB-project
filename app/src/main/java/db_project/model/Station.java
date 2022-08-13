7ac6472b439bf797a0c53d9104e10875a921f4bb
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-13 10:32:42 +0200

    Added location

--- app/src/main/java/db_project/model/Station.java
+++ app/src/main/java/db_project/model/Station.java
@@ -7,12 +7,14 @@ public class Station {
   private final String stationName;
   private final int rails;
   private final String managerCode;
+  private final String location;
 
-  public Station(String stationCode, String stationName, int rails, String managerCode) {
+  public Station(String stationCode, String stationName, int rails, String managerCode, final String location) {
     this.stationCode = stationCode;
     this.stationName = stationName;
     this.rails = rails;
     this.managerCode = managerCode;
+    this.location = location;
   }
 
   public String getStationCode() {
@@ -30,24 +32,22 @@ public class Station {
   public String getManagerCode() {
     return managerCode;
   }
+  
+  public String getLocation() {
+    return location;
+  }
 
   @Override
   public String toString() {
-    return "Station [managerCode="
-        + managerCode
-        + ", rails="
-        + rails
-        + ", stationCode="
-        + stationCode
-        + ", stationName="
-        + stationName
-        + "]";
+    return "Station [location=" + location + ", managerCode=" + managerCode + ", rails=" + rails + ", stationCode="
+        + stationCode + ", stationName=" + stationName + "]";
   }
 
   @Override
   public int hashCode() {
     final int prime = 31;
     int result = 1;
+    result = prime * result + ((location == null) ? 0 : location.hashCode());
     result = prime * result + ((managerCode == null) ? 0 : managerCode.hashCode());
     result = prime * result + rails;
     result = prime * result + ((stationCode == null) ? 0 : stationCode.hashCode());
@@ -57,20 +57,37 @@ public class Station {
 
   @Override
   public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null) return false;
-    if (getClass() != obj.getClass()) return false;
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
     Station other = (Station) obj;
+    if (location == null) {
+      if (other.location != null)
+        return false;
+    } else if (!location.equals(other.location))
+      return false;
     if (managerCode == null) {
-      if (other.managerCode != null) return false;
-    } else if (!managerCode.equals(other.managerCode)) return false;
-    if (rails != other.rails) return false;
+      if (other.managerCode != null)
+        return false;
+    } else if (!managerCode.equals(other.managerCode))
+      return false;
+    if (rails != other.rails)
+      return false;
     if (stationCode == null) {
-      if (other.stationCode != null) return false;
-    } else if (!stationCode.equals(other.stationCode)) return false;
+      if (other.stationCode != null)
+        return false;
+    } else if (!stationCode.equals(other.stationCode))
+      return false;
     if (stationName == null) {
-      if (other.stationName != null) return false;
-    } else if (!stationName.equals(other.stationName)) return false;
+      if (other.stationName != null)
+        return false;
+    } else if (!stationName.equals(other.stationName))
+      return false;
     return true;
   }
+
+  
 }
