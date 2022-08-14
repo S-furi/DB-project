d0c39afb7b677c04568bb643ea8dd062a7471e1b
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-14 11:33:12 +0200

    Changed classType from int to string

--- app/src/main/java/db_project/model/Seat.java
+++ app/src/main/java/db_project/model/Seat.java
@@ -3,19 +3,19 @@ package db_project.model;
 // Posto
 public class Seat {
 
-  private final int classType;
+  private final String classType;
   private final String trainCode;
   private final int carNumber;
   private final int seatNumber;
 
-  public Seat(int classType, String trainCode, int carNumber, int seatNumber) {
+  public Seat(String classType, String trainCode, int carNumber, int seatNumber) {
     this.classType = classType;
     this.trainCode = trainCode;
     this.carNumber = carNumber;
     this.seatNumber = seatNumber;
   }
 
-  public int getClassType() {
+  public String getClassType() {
     return classType;
   }
 
@@ -32,11 +32,17 @@ public class Seat {
   }
 
   @Override
+  public String toString() {
+    return "Seat [carNumber=" + carNumber + ", classType=" + classType + ", seatNumber=" + seatNumber + ", trainCode="
+        + trainCode + "]";
+  }
+
+  @Override
   public int hashCode() {
     final int prime = 31;
     int result = 1;
     result = prime * result + carNumber;
-    result = prime * result + classType;
+    result = prime * result + ((classType == null) ? 0 : classType.hashCode());
     result = prime * result + seatNumber;
     result = prime * result + ((trainCode == null) ? 0 : trainCode.hashCode());
     return result;
@@ -44,16 +50,27 @@ public class Seat {
 
   @Override
   public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null) return false;
-    if (getClass() != obj.getClass()) return false;
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
     Seat other = (Seat) obj;
-    if (carNumber != other.carNumber) return false;
-    if (classType != other.classType) return false;
-    if (seatNumber != other.seatNumber) return false;
+    if (carNumber != other.carNumber)
+      return false;
+    if (classType == null) {
+      if (other.classType != null)
+        return false;
+    } else if (!classType.equals(other.classType))
+      return false;
+    if (seatNumber != other.seatNumber)
+      return false;
     if (trainCode == null) {
-      if (other.trainCode != null) return false;
-    } else if (!trainCode.equals(other.trainCode)) return false;
+      if (other.trainCode != null)
+        return false;
+    } else if (!trainCode.equals(other.trainCode))
+      return false;
     return true;
   }
 }
