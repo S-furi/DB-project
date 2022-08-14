61105d9605eaf38d992f041e200811dca7eb6b71
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-14 11:10:56 +0200

    Added Table.update() test

--- app/src/test/java/db_project/db/tables/CarTableTest.java
+++ app/src/test/java/db_project/db/tables/CarTableTest.java
@@ -1,5 +1,6 @@
 package db_project.db.tables;
 
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -13,46 +14,58 @@ import db_project.db.ConnectionProvider;
 import db_project.model.Car;
 
 public class CarTableTest {
-  private static final String username = "root";
-  private static final String password = "123Test123";
-  private static final String dbName = "Ferrovia";
 
-  private static final ConnectionProvider connectionProvider =
-      new ConnectionProvider(username, password, dbName);
-  private static final CarTable carTable = new CarTable(connectionProvider.getMySQLConnection());
+    private static final String username = "root";
+    private static final String password = "123Test123";
+    private static final String dbName = "Ferrovia";
 
-  @BeforeAll
-  public static void setUp() {
-    carClassTableTest.setUp();
-    TrainTableTest.setUp();
+    private static final ConnectionProvider connectionProvider = 
+        new ConnectionProvider(username, password, dbName);
+    private static final CarTable carTable = 
+        new CarTable(connectionProvider.getMySQLConnection());
+    
+    @BeforeAll
+    public static void setUp() {
+        carClassTableTest.setUp();
+        TrainTableTest.setUp();
+        
+        final Car car1 = new Car("1", "1", 1, 50, false);
+        final Car car2 = new Car("1", "1", 2, 25, true);
 
-    final Car car1 = new Car("1", "1", 1, 50, false);
-    final Car car2 = new Car("1", "1", 2, 25, true);
+        assertTrue(carTable.save(car1));
+        assertTrue(carTable.save(car2));
+    }
 
-    assertTrue(carTable.save(car1));
-    assertTrue(carTable.save(car2));
-  }
+    @AfterAll
+    public static void tearDown() {
+        carTable.findAll().forEach(t -> carTable.delete(List.of(t.getClassType(), t.getTrainCode(), t.getPosition())));
+        TrainTableTest.tearDown();
+        carClassTableTest.tearDown();
+    }
 
-  @AfterAll
-  public static void tearDown() {
-    carTable
-        .findAll()
-        .forEach(
-            t -> carTable.delete(List.of(t.getClassType(), t.getTrainCode(), t.getPosition())));
-    TrainTableTest.tearDown();
-    carClassTableTest.tearDown();
-  }
+    @Test
+    public void findAll() {
+        assertFalse(carTable.findAll().isEmpty());
+        assertTrue(carTable.findAll().size() == 2);
+    }
 
-  @Test
-  public void findAll() {
-    assertFalse(carTable.findAll().isEmpty());
-    assertTrue(carTable.findAll().size() == 2);
-  }
+    @Test
+    public void testFindByPrimaryKey() {
+        assertTrue(carTable.findByPrimaryKey(List.of("1", "1", 1)).isPresent());
+        assertTrue(carTable.findByPrimaryKey(List.of("1", "1", 2)).isPresent());
+        assertFalse(carTable.findByPrimaryKey(List.of("1", "1", 3)).isPresent());       
+    }
 
-  @Test
-  public void testFindByPrimaryKey() {
-    assertTrue(carTable.findByPrimaryKey(List.of("1", "1", 1)).isPresent());
-    assertTrue(carTable.findByPrimaryKey(List.of("1", "1", 2)).isPresent());
-    assertFalse(carTable.findByPrimaryKey(List.of("1", "1", 3)).isPresent());
-  }
+
+    @Test
+    public void testUpdate() {
+        final var curr = carTable.findByPrimaryKey(List.of("1", "1", 1));
+        if (curr.isEmpty()) {
+            fail("Select Failed");
+        }
+        final var newCar = new Car("1", "1", 1, 500, true);
+
+        assertTrue(carTable.update(newCar));
+        assertTrue(carTable.update(curr.get()));
+    }
 }
