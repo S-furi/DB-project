76742ddfad7d8c4217223d3a516a64b5c4575500
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-15 15:33:12 +0200

    Changed implementation due to logical changes.

--- app/src/main/java/db_project/db/tables/TicketDetailTable.java
+++ app/src/main/java/db_project/db/tables/TicketDetailTable.java
@@ -8,62 +8,68 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.ArrayList;
 
-import db_project.db.AbstractTable;
+import db_project.db.AbstractCompositeKeyTable;
 import db_project.db.queryUtils.QueryResult;
 import db_project.model.TicketDetail;
 
-public class TicketDetailTable extends AbstractTable<TicketDetail, String> {
+public class TicketDetailTable extends AbstractCompositeKeyTable<TicketDetail, Object> {
   public static final String TABLE_NAME = "DETTAGLIO_BIGLIETTO";
-  public static final String PRIMARY_KEY = "codiceBiglietto";
+  public static final List<String> PRIMARY_KEY = List.of("numClasse", "codTreno", "numeroCarrozza", "numeroPosto", "dataViaggio");
   private final Logger logger;
-
+  
   public TicketDetailTable(final Connection connection) {
     super(TABLE_NAME, connection);
     super.setPrimaryKey(PRIMARY_KEY);
-    super.setTableColumns(
-        List.of("dataPrenotazione", "numClasse", "codTreno", "numeroCarrozza", "numeroPosto"));
-
-    this.logger = Logger.getLogger("CityTable");
+    super.setTableColumns(List.of("codiceBiglietto"));
+    this.logger = Logger.getLogger("TicketDetailTable");
     this.logger.setLevel(Level.WARNING);
   }
 
   @Override
-  protected Object[] getSaveQueryParameters(final TicketDetail ticketDetail) {
-    return new Object[] {
-      ticketDetail.getTicketId(),
-      ticketDetail.getReservationDate(),
-      ticketDetail.getTrainClass(),
-      ticketDetail.getTrainId(),
-      ticketDetail.getCarNumber(),
-      ticketDetail.getSeatNumber()
-    };
+  public boolean createTable() {
+    final String query = 
+    "create table DETTAGLIO_BIGLIETTO ( " 
+        + "numClasse varchar(1) not null, "
+        + "codTreno varchar(5) not null, "
+        + "numeroCarrozza int not null, "
+        + "numeroPosto int not null, "
+        + "dataViaggio date not null, "
+        + "codiceBiglietto varchar(5) not null, "
+        + "constraint ID_Prenotazione_ID primary key (numClasse, codTreno, numeroCarrozza, numeroPosto, dataViaggio), "
+        + "constraint FKRiseva_ID unique (codiceBiglietto)); ";
+    super.created = super.parser.computeSqlQuery(query, null);
+    return super.isCreated();
   }
 
+
   @Override
-  protected Object[] getUpdateQueryParameters(final TicketDetail ticketDetail) {
+  protected Object[] getSaveQueryParameters(final TicketDetail ticketDetail) {
     return new Object[] {
-      ticketDetail.getReservationDate(),
       ticketDetail.getTrainClass(),
       ticketDetail.getTrainId(),
       ticketDetail.getCarNumber(),
       ticketDetail.getSeatNumber(),
+      ticketDetail.getTripDate(),
       ticketDetail.getTicketId()
     };
   }
 
+
   @Override
-  public boolean createTable() {
-    final String query =
-        "create table DETTAGLIO_BIGLIETTO ( "
-            + "codiceBiglietto varchar(5) not null, "
-            + "dataPrenotazione date not null, "
-            + "numClasse int not null, "
-            + "codTreno varchar(5) not null, "
-            + "numeroCarrozza int not null, "
-            + "numeroPosto int not null, "
-            + "constraint FKRiseva_ID primary key (codiceBiglietto));";
-    super.created = super.parser.computeSqlQuery(query, null);
-    return super.isCreated();
+  protected Object[] getUpdateQueryParameters(final TicketDetail ticketDetail) {
+    return new Object[] {
+      ticketDetail.getTrainClass(),
+      ticketDetail.getTrainId(),
+      ticketDetail.getCarNumber(),
+      ticketDetail.getSeatNumber(),
+      ticketDetail.getTripDate(),
+      ticketDetail.getTicketId(),
+      ticketDetail.getTrainClass(),
+      ticketDetail.getTrainId(),
+      ticketDetail.getCarNumber(),
+      ticketDetail.getSeatNumber(),
+      ticketDetail.getTripDate()
+    };
   }
 
   @Override
@@ -78,12 +84,12 @@ public class TicketDetailTable extends AbstractTable<TicketDetail, String> {
         .forEach(
             row -> {
               logger.info(row.toString());
-              final String ticketId = (String) row.get("codiceBiglietto");
-              final Date reservationDate = (Date) row.get("dataPrenotazione");
               final String trainClass = (String) row.get("numClasse");
               final String trainId = (String) row.get("codTreno");
               final int carNumber = (int) row.get("numeroCarrozza");
               final int seatNumber = (int) row.get("numeroPosto");
+              final Date reservationDate = (Date) row.get("dataViaggio");
+              final String ticketId = (String) row.get("codiceBiglietto");
 
               ticketDetails.add(
                   new TicketDetail(
