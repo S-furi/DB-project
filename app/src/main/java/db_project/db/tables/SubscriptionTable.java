c3a75db1ae58b8022dd6017ea3080f6e8ca015ec
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-08-16 12:34:03 +0200

    Solved Conflict

--- app/src/main/java/db_project/db/tables/SubscriptionTable.java
+++ app/src/main/java/db_project/db/tables/SubscriptionTable.java
@@ -5,56 +5,13 @@ import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-<<<<<<< HEAD
-=======
 import java.util.logging.Level;
 import java.util.logging.Logger;
->>>>>>> 71986b79a7a39a3a8969efac5111e4d252ee3598
 
 import db_project.db.AbstractTable;
 import db_project.db.queryUtils.QueryResult;
 import db_project.model.Subscription;
 
-<<<<<<< HEAD
-public class SubscriptionTable extends AbstractTable<Subscription, String>{
-
-    public static final String TABLE_NAME = "SOTTOSCRIZIONE";
-    public static final String PRIMARY_KEY = "codPasseggero";
-
-    public SubscriptionTable(final Connection connection){
-        super(TABLE_NAME, connection);
-        super.setPrimaryKey(PRIMARY_KEY);
-        super.setTableColumns(List.of("codCarta", "dataSottoscrizione"));
-    }
-
-    @Override
-    protected Object[] getSaveQueryParameters(Subscription subscription) {
-        return new Object[]{subscription.getTravelerCode(), subscription.getCardNumber(), subscription.getSubscriptionDate()};
-    }
-
-    @Override
-    protected Object[] getUpdateQueryParameters(Subscription subscription) {
-        return new Object[]{subscription.getCardNumber(), subscription.getSubscriptionDate(), subscription.getTravelerCode()};
-    }
-
-    @Override
-    protected List<Subscription> getPrettyResultFromQueryResult(QueryResult result) {
-        if(result.getResult().isEmpty()){
-            return Collections.emptyList();
-        }
-        List<Subscription> subscriptions = new ArrayList<>();
-        result.getResult().get().forEach(row -> {
-            System.out.println(row.toString());
-            final String travelerCode = (String) row.get("codPasseggero");
-            final String cardNumber = (String) row.get("codCarta");
-            final Date subscriptionDate = (Date) row.get("dataSottoscrizione");
-            subscriptions.add(new Subscription(travelerCode, cardNumber, subscriptionDate));
-        });
-        return subscriptions;
-    }
-    
-    
-=======
 public class SubscriptionTable extends AbstractTable<Subscription, String> {
   public static final String TABLE_NAME = "SOTTOSCRIZIONE";
   public static final String PRIMARY_KEY = "codPasseggero";
@@ -119,5 +76,4 @@ public class SubscriptionTable extends AbstractTable<Subscription, String> {
             });
     return subscriptions;
   }
->>>>>>> 71986b79a7a39a3a8969efac5111e4d252ee3598
 }
