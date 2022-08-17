f6b28c4fa9183bfa268584df0d27a4ec640d8e96
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-17 14:38:17 +0200

    Now testing `getAllTripSolutions()`.

--- app/src/test/java/db_project/view/TestRailwayController.java
+++ app/src/test/java/db_project/view/TestRailwayController.java
@@ -2,17 +2,15 @@ package db_project.view;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 import db_project.db.dbGenerator.DBGenerator;
-import db_project.view.controller.DbRailwayController;
+import db_project.view.controller.PathController;
 
-@Ignore
 public class TestRailwayController {
   private static final DBGenerator dbGenerator = new DBGenerator();
-  private DbRailwayController railwayController;
+  private PathController railwayController;
 
   @AfterAll
   public static void tearDown() {
@@ -25,12 +23,16 @@ public class TestRailwayController {
     dbGenerator.createTables();
     dbGenerator.populateTables();
 
-    this.railwayController = new DbRailwayController(dbGenerator);
+    this.railwayController = new PathController(dbGenerator);
 
     final String srcStation = "BOLOGNA C.LE";
     final String dstStation = "BOLZANO";
     final var solution = this.railwayController.getTripSolution(srcStation, dstStation);
     assertTrue(solution.isPresent());
     System.out.println(solution.get());
+    final var solutions = this.railwayController.getAllTripSolutions();
+    assertFalse(solutions.isEmpty());
+    solutions.stream().map(t -> t.get()).forEach(System.out::println);
+    System.out.println(solutions.size());
   }
 }
