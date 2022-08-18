01d614090f4b17cef978d800f0bf055c230db588
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-08-18 14:54:13 +0200

    Implemented UserLandingController

---
+++ app/src/main/java/db_project/view/UserLandingController.java
@@ -0,0 +1,25 @@
+package db_project.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class UserLandingController {
+
+    @FXML
+    private Button buyTicket;
+
+    @FXML
+    private Button activeRoutes;
+
+    @FXML
+    private Button history;
+
+
+    public void initialize(URL location, ResourceBundle resurces){
+        
+    }
+
+}
