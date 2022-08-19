1465a83a7259f177279514b0ba34e9506bf6e478
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-19 16:27:49 +0200

    Implemented pathInfo and path creation from *All.json files.

--- app/src/main/java/db_project/view/adminPanelController/SectionController.java
+++ app/src/main/java/db_project/view/adminPanelController/SectionController.java
@@ -1,5 +1,7 @@
 package db_project.view.adminPanelController;
 
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,23 +14,36 @@ import db_project.db.dbGenerator.DBGenerator;
 import db_project.db.queryUtils.ArrayQueryParser;
 import db_project.db.queryUtils.QueryParser;
 import db_project.db.queryUtils.QueryResult;
-// import db_project.db.tables.SectionTable;
+import db_project.db.tables.AdminTable;
+import db_project.db.tables.PathInfoTable;
+import db_project.db.tables.PathTable;
+import db_project.db.tables.SectionTable;
 import db_project.model.Path;
+import db_project.model.PathInfo;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class SectionController {
+  private static final double AVG_STD_TRAIN_SPEED = 130.0;
+
   private final DBGenerator dbGenerator;
-  // private SectionTable sectionTable;
+  private SectionTable sectionTable;
+  private PathTable pathTable;
+  private PathInfoTable pathInfoTable;
+  private final PathController pathController;
   private ObservableList<PathDetail> pathDetails;
   private final QueryParser parser;
   private final Logger logger;
+  
+  private Path computedPath;
+  private List<PathInfo> pathInfosToBeSaved = new ArrayList<>();
 
-  public SectionController(final DBGenerator dbGenerator) {
+  public SectionController(final DBGenerator dbGenerator, final PathController pathController) {
     this.dbGenerator = dbGenerator;
-    // this.sectionTable = (SectionTable) this.dbGenerator.getTableByClass(SectionTable.class);
+    this.pathController = pathController;
+    this.initializeTables();
 
     this.parser =
         new ArrayQueryParser(this.dbGenerator.getConnectionProvider().getMySQLConnection());
@@ -39,6 +54,16 @@ public class SectionController {
     this.logger.setLevel(Level.WARNING);
   }
 
+  private void initializeTables() {
+    this.sectionTable = (SectionTable) this.dbGenerator.getTableByClass(SectionTable.class);
+    this.pathTable = (PathTable) this.dbGenerator.getTableByClass(PathTable.class);
+    this.pathInfoTable = (PathInfoTable) this.dbGenerator.getTableByClass(PathInfoTable.class);
+  }
+
+  /**
+   * 
+   * @param path that already exists in DB!!!
+   */
   public void computeSectionsFromPath(final Path path) {
     final var details = this.retreiveSections(path);
     if (details.isEmpty()) {
@@ -63,6 +88,79 @@ public class SectionController {
     this.parser.computeSqlQuery(query, new Object[] {path.getPathCode()});
     return Optional.of(this.getPathDetailsFromQuery(this.parser.getQueryResult()));
   }
+
+  /**
+   * 
+   * @param pathId that isn't contained into the database
+   * @return
+   */
+  public boolean findSolution(final String pathId) {
+    if (this.isPathAlreadyInDb(pathId)) {
+      return false;
+    }
+
+    final List<PathInfo> pathInfos = TripSolutionFinder.getPathInfosFromPathId(pathId);
+    if (pathInfos.isEmpty()) {
+      return false;
+    }
+    this.pathInfosToBeSaved = pathInfos;
+
+    return this.buildPathDetailsFromPathInfos(pathInfos);
+  }
+
+  private boolean isPathAlreadyInDb(final String pathId) {
+    return this.pathTable.findByPrimaryKey(pathId).isPresent();
+  }
+
+  private boolean buildPathDetailsFromPathInfos(final List<PathInfo> pathInfos) {
+    final String pathId = pathInfos.get(0).getPathId();
+
+    final List<String> sectionIds = 
+        pathInfos.stream().map(t -> t.getSectionId()).collect(Collectors.toList());
+
+    final int totalDistance = this.sectionTable.findAll()
+        .stream()
+        .filter(t -> sectionIds.contains(t.getSectionCode()))
+        .map(t -> t.getDistance())
+        .reduce(0, (t1, t2) -> t1 + t2);
+    
+    this.computedPath = new Path(pathId, this.getDurationFromDistance(totalDistance), sectionIds.size(), this.getRandomAdminId());
+  
+    this.buildNewPathDetail(pathId, pathInfos);
+
+    return !this.pathDetails.isEmpty();
+  }
+
+  private void buildNewPathDetail(String pathId, List<PathInfo> pathInfos) {
+    this.pathDetails.clear();
+    for (final var pathInfo : pathInfos) {
+      var stations = this.pathController.getStationsNameFromPathCode(pathInfo.getSectionId());
+      var srcStation = stations.getKey();
+      var dstStation = stations.getValue();
+      this.pathDetails.add(
+          new PathDetail(pathId, 
+              Integer.parseInt(pathInfo.getOrderNumber()), 
+              pathInfo.getSectionId(), 
+              srcStation, 
+              dstStation));
+    }
+  }
+
+  private String getRandomAdminId() {
+    return new AdminTable(this.dbGenerator.getConnectionProvider().getMySQLConnection()).getRandomAdminId();
+  }
+
+  private String getDurationFromDistance(final int distance) { 
+    return this.getDurationFromMinutes((int)((distance/AVG_STD_TRAIN_SPEED)*60));
+  }
+
+  private String getDurationFromMinutes(final long value) {
+    return LocalTime.MIN.plus(
+        Duration
+        .ofMinutes(value))
+        .toString();
+}
+
 
   private List<PathDetail> getPathDetailsFromQuery(final QueryResult result) {
     if (result.getResult().isEmpty()) {
@@ -112,7 +210,23 @@ public class SectionController {
   }
 
   public void clearPathDetails() {
+    this.computedPath = null;
     this.pathDetails.clear();
+    this.pathInfosToBeSaved.clear();
+  }
+
+  public boolean savePathInfosToDb() {
+    for (final var pathInfo : this.pathInfosToBeSaved) {
+      if (!this.pathInfoTable.save(pathInfo)) {
+        return false;
+      }
+    }
+    this.pathInfosToBeSaved.clear();
+    return true;
+  }
+
+  public Path getComputedPath() {
+    return this.computedPath;
   }
 
   public class PathDetail {
