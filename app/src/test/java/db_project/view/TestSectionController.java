2e1b9a0d14314cff8707d6abd04d1343f46591fb
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-19 16:28:35 +0200

    Changed constructor for SectionController

--- app/src/test/java/db_project/view/TestSectionController.java
+++ app/src/test/java/db_project/view/TestSectionController.java
@@ -10,11 +10,13 @@ import org.junit.jupiter.api.Test;
 import db_project.db.dbGenerator.DBGenerator;
 import db_project.db.tables.PathTable;
 import db_project.model.Path;
+import db_project.view.adminPanelController.PathController;
 import db_project.view.adminPanelController.SectionController;
 
 public class TestSectionController {
   private static final DBGenerator dbGenerator = new DBGenerator();
   private SectionController sectionController;
+  private PathController pathController;
   private PathTable pathTable;
 
   @AfterAll
@@ -27,8 +29,8 @@ public class TestSectionController {
     dbGenerator.createDB();
     dbGenerator.createTables();
     dbGenerator.populateTables();
-
-    this.sectionController = new SectionController(dbGenerator);
+    this.pathController = new PathController(dbGenerator);
+    this.sectionController = new SectionController(dbGenerator, this.pathController);
     this.pathTable = (PathTable) dbGenerator.getTableByClass(PathTable.class);
     final Optional<Path> routePath = this.pathTable.findByPrimaryKey("CA-LA");
     if (routePath.isEmpty()) {
