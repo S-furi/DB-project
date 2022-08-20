2bf1dbd0d8cc7c57bbba742d1ee817b2b43f36c3
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-20 13:43:27 +0200

    Implemented Rv tickets creation.

--- app/src/main/java/db_project/view/userPanelController/TicketBuyController.java
+++ app/src/main/java/db_project/view/userPanelController/TicketBuyController.java
@@ -10,10 +10,15 @@ import java.util.stream.Collectors;
 
 import db_project.db.dbGenerator.DBGenerator;
 import db_project.db.tables.RouteInfoTable;
+import db_project.db.tables.SeatTable;
 import db_project.db.tables.SubscriptionTable;
+import db_project.db.tables.TicketDetailTable;
 import db_project.db.tables.TicketTable;
+import db_project.db.tables.TrainTable;
 import db_project.model.RouteInfo;
+import db_project.model.Seat;
 import db_project.model.Ticket;
+import db_project.model.TicketDetail;
 import db_project.view.adminPanelController.PathController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -25,26 +30,35 @@ public class TicketBuyController {
   private static final Float KM_FEE = 0.09f;
   private ObservableList<TicketCheckout> tickets;
   private final DBGenerator dbGenerator;
-  private final TicketTable ticketTable;
-  private final RouteInfoTable routeInfoTable;
+  private TicketTable ticketTable;
+  private TicketDetailTable ticketDetailTable;
+  private RouteInfoTable routeInfoTable;
+  private SeatTable seatTable;
   private final Logger logger;
 
   public TicketBuyController(DBGenerator dbGenerator) {
-    this.tickets = FXCollections.observableArrayList();
     this.dbGenerator = dbGenerator;
-    this.ticketTable = (TicketTable) this.dbGenerator.getTableByClass(TicketTable.class);
-    this.routeInfoTable = (RouteInfoTable) this.dbGenerator.getTableByClass(RouteInfoTable.class);
+    this.initializeTables();
+    this.tickets = FXCollections.observableArrayList();
     this.logger = Logger.getLogger("TicketController");
-    this.logger.setLevel(Level.INFO);
+    this.logger.setLevel(Level.WARNING);
+  }
+
+  private void initializeTables() {
+    this.ticketTable = (TicketTable) this.dbGenerator.getTableByClass(TicketTable.class);
+    this.ticketDetailTable = (TicketDetailTable) this.dbGenerator.getTableByClass(TicketDetailTable.class);
+    this.routeInfoTable = (RouteInfoTable) this.dbGenerator.getTableByClass(RouteInfoTable.class);
+    this.seatTable = (SeatTable) this.dbGenerator.getTableByClass(SeatTable.class);
   }
 
   public boolean registerTicketBought(
       final Date date,
       final String pathId,
       final String trainId,
-      final boolean isRv,
       final boolean isFirstClass) {
 
+    final boolean isRv = this.checkTrainRv(trainId);
+      
     return isRv
         ? this.saveRvTicket(date, pathId, trainId, isFirstClass)
         : this.saveStdTicket(date, pathId, trainId);
@@ -71,17 +85,56 @@ public class TicketBuyController {
 
   private boolean saveRvTicket(
       final Date date, final String pathId, final String trainId, final boolean isFirstClass) {
-    return false;
+    
+    final var routeInfo = this.getRouteInfo(pathId, trainId, date);
+    if (routeInfo.isEmpty()) {
+        return false;
+    }
+    //tmp
+    final var usrId = this.getRandomUserId();
+    final Float price = this.computePrice(usrId, pathId) * 1.15f;
+
+    final Ticket ticket =
+        new Ticket(this.getLastTicketId(), true, Optional.empty(), usrId, price, routeInfo.get());
+    final Optional<TicketDetail> ticketDetail = this.buildTicketDetail(ticket, routeInfo.get(), isFirstClass);
+
+    if (ticketDetail.isEmpty()) {
+        return false;
+    }
+
+    this.logger.info(ticket.toString());
+    this.logger.info(ticketDetail.toString());
+
+    this.tickets.clear();
+    this.tickets.add(
+        new TicketCheckout(ticket.getTicketId(), true, routeInfo.get().getPathId(), ticketDetail.get().getTrainClass(),
+            ticketDetail.get().getCarNumber(), ticketDetail.get().getSeatNumber(), ticketDetail.get().getTripDate()));
+
+    return this.ticketTable.save(ticket) && this.ticketDetailTable.save(ticketDetail.get());
+  }
+
+  private Optional<TicketDetail> buildTicketDetail(final Ticket ticket, final RouteInfo routeInfo, final boolean isFirstClass) {
+    final var seat = this.getFirstAvailableSeat(routeInfo, isFirstClass ? "1" : "2");
+    if (seat.isEmpty()) {
+      return Optional.empty();
+    }
+    return Optional.of(TicketDetail.getTicketDetailFromSeat(ticket.getTicketId(), routeInfo.getDate(), seat.get()));
+  }
+
+  private Optional<Seat> getFirstAvailableSeat(final RouteInfo routeInfo, final String carClass) {
+    if (this.ticketDetailTable.getTicketDetailFromRouteInfo(routeInfo, carClass).isEmpty()) {
+      return this.seatTable.getFirstSeat(routeInfo, carClass);
+    }
+    return this.seatTable.getFirstAvailableSeat(routeInfo, carClass);
   }
 
   private Float computePrice(final String usrId, final String pathId) {
-    final int discount =
-        ((SubscriptionTable) this.dbGenerator.getTableByClass(SubscriptionTable.class))
-            .getDiscountPassengersPercentages()
-            .get(usrId);
+    final int discount = ((SubscriptionTable) this.dbGenerator.getTableByClass(SubscriptionTable.class))
+        .getDiscountPassengersPercentages()
+        .get(usrId);
 
-    final int totalDistance =
-        new PathController(this.dbGenerator).getTripSolutionFromPathId(pathId).get().getDistance();
+    final int totalDistance = new PathController(this.dbGenerator).getTripSolutionFromPathId(pathId).get()
+        .getDistance();
     return (totalDistance * KM_FEE) - ((totalDistance * KM_FEE) * discount) / 100;
   }
 
@@ -142,6 +195,18 @@ public class TicketBuyController {
 
   public ObservableList<TicketCheckout> getBoughtTicketDetails() {
     return this.tickets;
+  }
+
+  public void restoreLastOp() {
+    if (this.tickets.isEmpty()) {
+      return;
+    }
+    final var ticket = this.tickets.get(0);
+    if (ticket.getIsRv()) {
+      if (this.ticketDetailTable.getTicketDetailFromTicketId(ticket.getTicketId()).isEmpty()) {
+        this.ticketTable.delete(ticket.getTicketId());
+      }
+    }
   }
 
   public class TicketCheckout {
@@ -234,5 +299,10 @@ public class TicketBuyController {
         ((SubscriptionTable) this.dbGenerator.getTableByClass(SubscriptionTable.class))
             .findAll().stream().map(t -> t.getPassengerCode()).collect(Collectors.toList());
     return lst.get(new Random().nextInt(lst.size() - 1));
+  }
+
+  public boolean checkTrainRv(final String trainId) {
+    if (trainId == null) return false;
+    return ((TrainTable) this.dbGenerator.getTableByClass(TrainTable.class)).findByPrimaryKey(trainId).get().getIsRv();
   }
 }
