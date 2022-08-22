7eba56378c3ccd7be567a6b314a805b75c7ca82e
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-08-22 10:13:53 +0200

    Implemented Authenticator

---
+++ app/src/main/java/db_project/utils/Authenticator.java
@@ -0,0 +1,37 @@
+package db_project.utils;
+
+import java.util.List;
+
+public class Authenticator {
+    
+
+    
+    public static boolean authenticate(String email, String password){
+
+        final List<Credentials> credentials = new AbstractJsonReader<Credentials>() {}.setFileName("DbAuth.json").retreiveData(Credentials.class);
+        return credentials.stream().filter(t -> t.getEmail().equals(email)).filter(t -> t.getPassword().equals(password)).findAny().isPresent();
+    }
+
+
+    private class Credentials{
+
+        private final String email;
+        private final String password;
+
+
+        public Credentials(String email, String password){
+            this.email = email;
+            this.password = password;
+        } 
+
+        public String getEmail(){
+            return this.email;
+        }
+
+        public String getPassword(){
+            return this.password;
+        }
+
+    }
+
+}
