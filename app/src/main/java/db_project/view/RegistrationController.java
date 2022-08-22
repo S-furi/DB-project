e2336d449ecbd25cb30cac1081dd6e2290aac566
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-08-22 13:57:56 +0200

    Now Saving new users

--- app/src/main/java/db_project/view/RegistrationController.java
+++ app/src/main/java/db_project/view/RegistrationController.java
@@ -1,10 +1,13 @@
 package db_project.view;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
+
+import javax.swing.Action;
 
 import db_project.db.ConnectionProvider;
 import db_project.db.tables.CityTable;
@@ -12,12 +15,17 @@ import db_project.db.tables.PassengerTable;
 import db_project.model.Passenger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class RegistrationController implements Initializable {
 
@@ -110,10 +118,10 @@ public class RegistrationController implements Initializable {
       this.phoneNumReg.clear();
       this.emailReg.clear();
     }
-    this.executeData(data);
+    this.executeData(data, event);
   }
 
-  private void executeData(List<String> data) {
+  private void executeData(List<String> data, ActionEvent event) {
     var newID = passengerTable.getHighestID() + 1;
     var newUser =
         new Passenger(
@@ -124,8 +132,8 @@ public class RegistrationController implements Initializable {
             data.get(3),
             data.get(6)
         );
-    System.out.println(newUser.toString());
-    System.out.println(Arrays.toString(data.toArray()));
+    passengerTable.save(newUser);
+    this.switchToLogIn(event);
   }
 
   private boolean validateData() {
@@ -143,5 +151,17 @@ public class RegistrationController implements Initializable {
               this.cittReg.getItems().add(t.getName());
             });
     System.out.println("ChoiceBox setup went fine");
+  }
+
+  private void switchToLogIn(ActionEvent event){
+    try {
+      Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
+      var stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      var scene = new Scene(root);
+      stage.setScene(scene);
+      stage.show();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
