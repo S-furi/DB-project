3caeeece1bd62694f8edcbfa54b003464deb4d87
Stefano Furi <stefano.furi@studio.unibo.it>
2022-08-22 10:36:18 +0200

    Implemented authentication responses.

--- app/src/main/java/db_project/utils/Authenticator.java
+++ app/src/main/java/db_project/utils/Authenticator.java
@@ -2,28 +2,45 @@ package db_project.utils;
 
 import java.util.List;
 
+import db_project.utils.authentication.AuthResponses;
+
 public class Authenticator {
 
-  public static boolean authenticate(String email, String password) {
+  public static AuthResponses authenticate(String email, String password) {
 
     final List<Credentials> credentials =
         new AbstractJsonReader<Credentials>() {}.setFileName("DbAuth.json")
             .retreiveData(Credentials.class);
-    return credentials.stream()
+    if (
+      credentials.stream()
         .filter(t -> t.getEmail().equals(email))
         .filter(t -> t.getPassword().equals(password))
         .findAny()
-        .isPresent();
+        .isEmpty()
+        ) {
+      return AuthResponses.DENIED;
+    }
+
+    final var privileges = credentials.stream().findFirst().get().getPrivileges();
+    if (AuthResponses.ROOT.equals(privileges)) {
+      return AuthResponses.ROOT;
+    } else if (AuthResponses.USER.equals(privileges)) {
+      return AuthResponses.USER;
+    } else {
+      return AuthResponses.DENIED;
+    }
   }
 
   public class Credentials {
 
     private final String email;
     private final String password;
+    private final String privileges;
 
-    public Credentials(String email, String password) {
+    public Credentials(final String email, final String password, final String privileges) {
       this.email = email;
       this.password = password;
+      this.privileges = privileges;
     }
 
     public String getEmail() {
@@ -33,5 +50,12 @@ public class Authenticator {
     public String getPassword() {
       return this.password;
     }
+
+    public String getPrivileges() {
+      return this.privileges;
+    }
+
+    // public boolean equals(final Credentials credentials) {
+    // }
   }
 }
