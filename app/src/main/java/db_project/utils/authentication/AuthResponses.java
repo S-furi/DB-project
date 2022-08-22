4a356861cafdb320283568d280e0fb0b4a1c37bd
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-08-22 10:23:16 +0200

    Populate Responses

--- app/src/main/java/db_project/utils/authentication/AuthResponses.java
+++ app/src/main/java/db_project/utils/authentication/AuthResponses.java
@@ -1,9 +1,5 @@
 package db_project.utils.authentication;
 
-<<<<<<< HEAD
 public enum AuthResponses {
     USER, ROOT, DENIED
 }
-=======
-public enum AuthResponses {}
->>>>>>> a56f99d4d47614e6fcc142bfd7faababf8a2f390
